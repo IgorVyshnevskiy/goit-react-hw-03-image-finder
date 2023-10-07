b64e66a46ccbb841dcf42400f9109bd026242126
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-10-07 22:20:40 +0300

    added styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import css from './Loader.module.css';
 const Loader = () => {
   return (
     <div className={css.containerLoader}>
-      <TailSpin color="#f27b77" height={50} width={50} />
+      <TailSpin color="#77b0f2" height={50} width={50} />
       <p className={css.item}>Завантаження...</p>
     </div>
   );
